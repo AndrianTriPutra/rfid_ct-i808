2cfe105b846b9b8338f3d9acf92e8c595a265ac5
kecapi <andrian.putra@miota.io>
2022-11-18 14:25:25 +0700

    port

--- socat.sh
+++ socat.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sleep 3
-stty -F /dev/ttyS0 9600 cs7 -cstopb -parenb
+stty -F /dev/ttyUSB0 9600 cs7 -cstopb -parenb
 sleep 2
-socat TCP-LISTEN:8080,fork,reuseaddr FILE:/dev/ttyS0,b57600,raw
+socat TCP-LISTEN:8080,fork,reuseaddr FILE:/dev/ttyUSB0,b57600,raw
